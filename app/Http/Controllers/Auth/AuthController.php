73a24954c3695b9e3d42ea9c8f348ed48e03b2f0
Mounir Hamzaoui <mounirhamzaoui@iMac-de-admin.home>
2017-03-13 11:18:15 +0100

    Update
    replacing undefined method getAccessToken with getTokenField

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -129,7 +129,7 @@ class AuthController extends Controller
         return response()->success($user);
       }else if($provider=='google'){
         //the accessToken param received in this case is the serverAuthCode
-        $aTokenFromServerAuth=Socialite::driver($provider)->getAccessToken($accessToken);
+        $aTokenFromServerAuth=Socialite::driver($provider)->getTokenFields($accessToken);
         $oauthUser = Socialite::driver($provider)->userFromToken($aTokenFromServerAuth);
         $user = $this->findOrCreateUser($oauthUser,$provider);
         return response()->success($user);
