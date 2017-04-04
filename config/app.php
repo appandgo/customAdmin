7d8e22e5b0e4bb9a1b3c5833eee2e9197496a5f0
Mounir Hamzaoui <mounirhamzaoui@iMac-de-mounir.home>
2017-04-04 18:16:15 +0200

    Updated
    Add Firebase Cloud Messaging
    Update Laravel Socialite to 3.0.0

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         Laravel\Socialite\SocialiteServiceProvider::class,
         Bican\Roles\RolesServiceProvider::class,
         LaravelFCM\FCMServiceProvider::class,
+        Illuminate\Notifications\NotificationServiceProvider::class,
 
     ],
 
